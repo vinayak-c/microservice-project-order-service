3000c1deea6676157e1443e599a3d03149131a4c
Vinayak <vinayakchaurasia99@gmail.com>
2021-08-08 21:16:13 +0530

    Added model fields

--- src/main/java/com/vinayak/microservice/orderservice/model/Order.java
+++ src/main/java/com/vinayak/microservice/orderservice/model/Order.java
@@ -1,4 +1,21 @@
 package com.vinayak.microservice.orderservice.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Data
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItems;
 }
