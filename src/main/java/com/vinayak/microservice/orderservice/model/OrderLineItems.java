366b55dba9713118bb8f44dfb3f8396b44c27a38
Vinayak <vinayakchaurasia99@gmail.com>
2021-08-08 21:15:24 +0530

    Added OrderLineItems model

---
+++ src/main/java/com/vinayak/microservice/orderservice/model/OrderLineItems.java
@@ -0,0 +1,20 @@
+package com.vinayak.microservice.orderservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "order_list_items")
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
