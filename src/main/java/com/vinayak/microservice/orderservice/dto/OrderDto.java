c816f4a4c2df4489a6af88e3f93a14d7cf3d0ac4
Vinayak <vinayakchaurasia99@gmail.com>
2021-08-08 21:26:29 +0530

    Added fields

--- src/main/java/com/vinayak/microservice/orderservice/dto/OrderDto.java
+++ src/main/java/com/vinayak/microservice/orderservice/dto/OrderDto.java
@@ -1,4 +1,16 @@
 package com.vinayak.microservice.orderservice.dto;
 
+import com.vinayak.microservice.orderservice.model.OrderLineItems;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
 public class OrderDto {
+
+    private List<OrderLineItems> orderLineItems;
 }
