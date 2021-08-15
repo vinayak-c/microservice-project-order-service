c7736d34d0d63e81a01bf870a7195e7371dc9e3b
Vinayak <vinayakchaurasia99@gmail.com>
2021-08-16 01:13:27 +0530

    Added resilince

--- src/main/java/com/vinayak/microservice/orderservice/controller/OrderController.java
+++ src/main/java/com/vinayak/microservice/orderservice/controller/OrderController.java
@@ -1,16 +1,20 @@
 package com.vinayak.microservice.orderservice.controller;
 
+import com.vinayak.microservice.orderservice.client.InventoryClient;
 import com.vinayak.microservice.orderservice.dto.OrderDto;
 import com.vinayak.microservice.orderservice.model.Order;
 import com.vinayak.microservice.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/api/order")
@@ -19,14 +23,33 @@ import java.util.UUID;
 public class OrderController {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
     @PostMapping
-    public String placeOrder(@RequestBody OrderDto orderDto){
-        Order order = new Order();
-        order.setOrderLineItems(orderDto.getOrderLineItems());
-        order.setOrderNumber(UUID.randomUUID().toString());
+    public String placeOrder(@RequestBody OrderDto orderDto) {
 
-        orderRepository.save(order);
-        return "Order Placed Successfully";
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("inventory");
+
+        Supplier<Boolean> booleanSupplier = () -> orderDto.getOrderLineItems().stream()
+                .allMatch(orderLineItems -> inventoryClient.checkStock(orderLineItems.getSkuCode()));
+
+        boolean allProductsInStock = circuitBreaker.run(booleanSupplier, throwable -> handleErrorCase());
+
+        if (allProductsInStock) {
+            Order order = new Order();
+            order.setOrderLineItems(orderDto.getOrderLineItems());
+            order.setOrderNumber(UUID.randomUUID().toString());
+
+            orderRepository.save(order);
+            return "Order Placed Successfully";
+        } else {
+            return "Order failed, some items not in stock";
+        }
+
+    }
+
+    private Boolean handleErrorCase() {
+        return false;
     }
 }
